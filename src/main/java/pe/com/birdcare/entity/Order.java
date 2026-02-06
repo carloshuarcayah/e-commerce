3f30ad489d37dcbe2b65510cdc37e339f00a0b96
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-02-06 11:31:27 -0500

    feat(enum): create OrderStatus enum and add status property

--- src/main/java/pe/com/birdcare/entity/Order.java
+++ src/main/java/pe/com/birdcare/entity/Order.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
+import pe.com.birdcare.enums.OrderStatus;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -37,4 +38,10 @@ public class Order implements Serializable {
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> items;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, nullable = false)
+
+    @Builder.Default
+    private OrderStatus status = OrderStatus.PENDING; // Set a default!
 }
---
+++ src/main/java/pe/com/birdcare/enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package pe.com.birdcare.enums;
+
+public enum OrderStatus {
+    PENDING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
