13b20946876c0ee15379675a34bb5b19fec9fd37
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-02-11 01:49:22 -0500

    feat(repo): add findByEmail to UserRepository

--- src/main/java/pe/com/birdcare/repository/UserRepository.java
+++ src/main/java/pe/com/birdcare/repository/UserRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pe.com.birdcare.entity.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Page<User>findAllByActiveTrue(Pageable pageable);
     Page<User>findAllByNameContainingIgnoreCase(Pageable pageable,String name);
+
+    Optional<User> findByEmail(String email);
 }
